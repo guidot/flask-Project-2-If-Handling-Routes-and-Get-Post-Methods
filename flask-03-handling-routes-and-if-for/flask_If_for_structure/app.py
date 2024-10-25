6963d1935eb8b9d1c5129f79b81a6143c1a858c9
guidot <6181359+guidot@users.noreply.github.com>
2024-10-25 13:09:09 +0200

    Coding style clean up, playing with name display

--- flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -1,27 +1,31 @@
-# Import Flask modules
 from flask import Flask, render_template
-# Create an object named app 
+
+# Create an object named app
 app = Flask(__name__)
 
-# Create a function named head which shows the message as "This is my first conditions experience" in `index.html` 
-# and assign to the route of ('/')
+
 @app.route("/")
 def head():
-    first="This is my first conditions experience"
+    """head() shows the message "first" in index.html
+    and assigns it to the route of ('/')
+    """
+    first = "Test conditions"
     return render_template("index.html", message=first)
+    # return render_template("index.html")
 
 
-# Create a function named header which prints numbers elements of list one by one in `index.html` 
-# and assign to the route of ('/')
 @app.route("/list")
 def header():
-    # names =["tom", "osvaldo", "guile", "tommy"]
-    numbers = range (1,11)
-    return render_template("body.html", object = numbers)
+    """header() prints elements of  a list
+    one by one in index.html
+    and assigns it to the route of ('/')
+    """
+    all_names = ["Anja", "Bob", "Charlotte", "Devil", "Erika", "Fabiano"]
+    # numbers = range(1, 11)
+    return render_template("body.html", names=all_names)
 
 
-# run this app in debug mode on your local.
-if __name__== "__main__":
+# run this app in debug mode on your local box
+if __name__ == "__main__":
     app.run(debug=True)
     # app.run(host='0.0.0.0', port=8081)
-
